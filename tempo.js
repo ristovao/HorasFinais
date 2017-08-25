9256a672039461d5c61b5099878a47720cae0c7f
Cristovao <cristovao.frinhani@gmail.com>
2017-08-24 21:39:53 -0300

    Serapendo html e javascrit

---
+++ tempo.js
@@ -0,0 +1,14 @@
+var repetir = setInterval(tempo ,1000);
+function tempo() {
+    var dataAgora = new Date();
+    var dataFinal = new Date("12/9/2017 GMT-0300");
+    var diferenca = Math.abs(dataFinal.getTime() - dataAgora.getTime());
+    var dias = Math.floor(diferenca / (1000 * 3600 * 24));
+    var horas = Math.floor(diferenca / (1000 * 3600 ))%24;
+    var minutos = Math.floor(diferenca / (1000 * 60))%60;
+    var segundos = Math.floor(diferenca / (1000))%60;
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas ;
+    document.getElementById("minutos").innerHTML = minutos ;
+    document.getElementById("segundos").innerHTML = segundos ;
+}
